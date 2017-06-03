913efaad8ee13021518cc17f6ae524cf9bfbbaf7
Nicole Sattler <nicole.sattler@live.com>
2017-06-03 00:45:24 +0000

    Switched images and gifs to lower quality for faster loading

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ $(document).ready(function(){
   $('#searchBar').keyup(function(){
     $('#results').empty();
     var searchTerm = $('#searchBar').val();
-    var url = 'https://api.giphy.com/v1/gifs/search?&q='+searchTerm+'&api_key=dc6zaTOxFJmzC';
+    var url = 'https://api.giphy.com/v1/gifs/search?&q='+searchTerm+'&limit=10&api_key=dc6zaTOxFJmzC';
     
     $.getJSON(url, function(giphy){
       for(var i = 0; i < giphy.data.length; i++) {
-        $('#results').append('<li><a href="'+giphy.data[i].images.original.url+'" target="_blank"><img class="showing" src="'+giphy.data[i].images.original_still.url+'"><img class="hidden" src="'+giphy.data[i].images.original.url+'"></a></li>');
+        $('#results').append('<li><a href="'+giphy.data[i].images.original.url+'" target="_blank"><img class="showing" src="'+giphy.data[i].images.downsized_still.url+'"><img class="hidden" src="'+giphy.data[i].images.preview_gif.url+'"></a></li>');
       }
     });
   });
-  $('#results').on('mouseenter','li', function(){
-    $(this).find('.hidden').show();
-    $(this).find('.showing').hide();
+  $('#results').on('mouseenter','img', function(){
+    $(this).parent().find('.hidden').show();
+    $(this).parent().find('.showing').hide();
   });
   $('#results').on('mouseleave','li', function(){
     $(this).find('.hidden').hide();
