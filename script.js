7ddd902f6c4988f4115b54c7ff5ccb9b7c6ebffd
Nicole Sattler <nicole.sattler@live.com>
2017-06-03 16:37:11 +0000

    Mobile funtionality - Added iframe for viewing

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 /* global $ */
 
 $(document).ready(function(){
+  
+  window.oncontextmenu = function(event) {
+     event.preventDefault();
+     event.stopPropagation();
+     return false;
+};
+  
   $('#searchBar').off();
   $('#searchBar').keyup(function(){
     $('#results').empty();
@@ -9,30 +16,43 @@ $(document).ready(function(){
     
     $.getJSON(url, function(giphy){
       for(var i = 0; i < giphy.data.length; i++) {
-        $('#results').append('<li><a href="'+giphy.data[i].images.original.url+'" target="_blank"><img class="showing" src="'+giphy.data[i].images.downsized_still.url+'"><img class="hidden" src="'+giphy.data[i].images.preview_gif.url+'"></a></li>');
+         $('#results').append('<li><img class="showing" src="'+giphy.data[i].images.downsized_still.url+'"><img class="hidden" src="'+giphy.data[i].images.preview_gif.url+'"></li>');
       }
     });
   });
   
-  if($(window).width() <= 420) {
-    $('#results').on('click taphold','img', function(){
-      $(this).parent().find('.hidden').show();
-      $(this).parent().find('.showing').hide();
-      return false;
-    });
-    $('#results').on('mouseleave','li', function(){
-      $(this).find('.hidden').hide();
-      $(this).find('.showing').show();
-    });
-   } else {
+    //Show preview gif on mouseenter or taphold
     $('#results').on('mouseenter taphold','img', function(){
       $(this).parent().find('.hidden').show();
       $(this).parent().find('.showing').hide();
     });
+    
+    //Stop preview gif on mouseleave
     $('#results').on('mouseleave','li', function(){
       $(this).find('.hidden').hide();
       $(this).find('.showing').show();
     });
+  
+  //Open gif in iframe on smaller devices
+  if($(window).width() <= 640){
+    $('#results').on('click', 'img', function(e){
+    var link = $(this).parent().find('.hidden').attr('src');
+    console.log(link);
+  $('iframe').attr('src', link);  
+  $('#iframeContain').show(); 
+  });
+  
+  $('#close').on('click', function(){
+    $('#iframeContain').hide();
+    $('iframe').attr('src', '');  
+  });
+  } else {
+     //Turn whole li into link and open in new tab
+    $('#results').on('click','img', function(){
+      var link = $(this).parent().find('.hidden').attr('src');
+      window.open(link);
+    });
+ 
   }
   
 });
